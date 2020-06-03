367c56c98e1c0f93d560530cca6731922f61c9f3
Nicholas Blotti <nblotti@gmail.com>
2020-06-03 18:12:57 +0200

    search serialization

--- src/main/java/ch/nblotti/asset/firm/FirmController.java
+++ src/main/java/ch/nblotti/asset/firm/FirmController.java
@@ -1,6 +1,14 @@
 package ch.nblotti.asset.firm;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.TypeRef;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -11,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/firm")
@@ -22,16 +32,31 @@ public class FirmController {
   @Value("${solr.server.uri}")
   private String searchServerHost;
 
+  public String highlightStr = "$.response.docs";
 
   @PostMapping(value = "/search/")
-  public String mirrorRest(@RequestBody String body, @RequestHeader MultiValueMap<String, String> headers) throws URISyntaxException {
+  public List<FirmSearchDto> mirrorRest(@RequestBody String body, @RequestHeader MultiValueMap<String, String> headers) throws URISyntaxException {
 
     String url = String.format("%s", searchServerHost);
 
     ResponseEntity<String> responseEntity =
       restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<String>(body, headers), String.class);
 
-    return responseEntity.getBody();
+    Configuration conf = Configuration
+      .builder()
+      .mappingProvider(new JacksonMappingProvider())
+      .jsonProvider(new JacksonJsonProvider())
+      .build();
+
+    TypeRef<List<FirmSearchDto>> type = new TypeRef<List<FirmSearchDto>>() {
+    };
+
+    List<FirmSearchDto> firms = JsonPath
+      .using(conf)
+      .parse(responseEntity.getBody())
+      .read(highlightStr, type);
+
+    return firms;
 
 
   }
