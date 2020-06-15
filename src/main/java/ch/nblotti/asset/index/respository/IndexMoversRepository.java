4f625d99161d7295be5c69d2a5b57ec9a397e32d
Nicholas Blotti <nblotti@gmail.com>
2020-06-15 21:39:16 +0200

    indices

--- src/main/java/ch/nblotti/asset/index/respository/IndexMoversRepository.java
+++ src/main/java/ch/nblotti/asset/index/respository/IndexMoversRepository.java
@@ -4,12 +4,7 @@ import ch.nblotti.asset.common.ReadOnlyRepository;
 import ch.nblotti.asset.index.to.IndexMoversTO;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "indexmovers")
+@RepositoryRestResource(path = "indices")
 public interface IndexMoversRepository extends ReadOnlyRepository<IndexMoversTO, String> {
 
-  Iterable<IndexMoversTO> findFirst10ByOrderByVolumeDesc();
-
-  Iterable<IndexMoversTO> findFirst10ByOrderByPercentChangeDesc();
-
-  Iterable<IndexMoversTO> findFirst10ByOrderByPercentChangeAsc();
 }
