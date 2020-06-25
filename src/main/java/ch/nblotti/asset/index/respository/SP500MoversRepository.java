c6b66e8e23de01ffcb050c72120ba90abe723755
Nicholas Blotti <nblotti@gmail.com>
2020-06-25 09:09:08 +0200

    sp 3 or 10 top

--- src/main/java/ch/nblotti/asset/index/respository/SP500MoversRepository.java
+++ src/main/java/ch/nblotti/asset/index/respository/SP500MoversRepository.java
@@ -11,29 +11,29 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface SP500MoversRepository extends ReadOnlyRepository<SP500MoversTO, Integer> {
 
   @Cacheable("findFSPirst10TopLoosers")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =1")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =1 and sp.myRank <= 10")
   Iterable<SP500MoversTO> findFSPirst10TopLoosers();
 
 
   @Cacheable("findFSPirst3TopLoosers")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =1")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =1 and sp.myRank <= 3")
   Iterable<SP500MoversTO> findFSPirst3TopLoosers();
 
   @Cacheable("findFSPirst10TopWiners")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =2")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =2 and sp.myRank <= 10")
   Iterable<SP500MoversTO> findFSPirst10TopWiners();
 
 
   @Cacheable("findFSPirst3TopWiners")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =2")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =2 and sp.myRank <= 3")
   Iterable<SP500MoversTO> findFSPirst3TopWiners();
 
   @Cacheable("findFSPirst10TopMovers")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =3")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =3 and sp.myRank <= 10")
   Iterable<SP500MoversTO> findFSPirst10TopMovers();
 
   @Cacheable("findFSPirst3TopMovers")
-  @Query("select sp from SP500MoversTO sp where sp.viewType =3")
+  @Query("select sp from SP500MoversTO sp where sp.viewType =3 and sp.myRank <= 3")
   Iterable<SP500MoversTO> findFSPirst3TopMovers();
 
 
